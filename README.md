35dc47de670ad7de58bbb9c7ee7057067521604a
tjsm <mathincosmos@gmail.com>
2024-12-31 08:05:33 +0530

    create encoding fn

---
+++ README.md
@@ -0,0 +1,17 @@
+# EncodeBase62
+
+Encode your integer into base62 or generate a random base62 encoded string
+
+### Installation
+
+```js
+npm i encodebase62
+```
+
+### Usage
+
+```js
+const encodeBase62 = require('encodebase62');
+encodeBase62(); // return 6 digits base62 encoded string
+encodeBase62(12345); // convert 12345 to base62
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+function encodeBase62(n = null, length = 6) {
+  let maxRange;
+
+  if (length) maxRange = Math.pow(62, length) - 1;
+  else maxRange = Math.pow(62, 6) - 1;
+  if (n && length && (n > maxRange || n < 0)) {
+    console.error(`Integer should be in range [0,${maxRange}]`);
+    return;
+  }
+
+  let res = '';
+  if (!n) {
+    n = Math.random() * maxRange;
+  }
+
+  while (n > 0) {
+    res += chars[n % 62];
+    n = Math.floor(n / 62);
+  }
+
+  return res.padStart(6, '0').split('').reverse().join('');
+}
+
+module.exports = encodeBase62;
